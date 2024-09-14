f3259434195b95574709ac8906f31c0ee1f61f22
Amit Rawat <167463184+AmitRawat99@users.noreply.github.com>
2024-09-14 20:34:56 +0530

    simple  commit

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+// let MenuBtn = document.getElementById('bars');
+// MenuBtn.addEventListener ('click',function(e){
+//     document.querySelector('body').classList.toggle('mobile-now-active');
+//     this.classList.toggle('fa-xmark')
+// })
+
+let MenuBtn = document.getElementById('bars');
+MenuBtn.addEventListener('click',function(e){
+   document.querySelector('body').classList.toggle('mobile-now-active');
+   this.classList.toggle('fa-xmark');
+})
+
+
+// const ulList = document.getElementById('ulList')
+// const cursor = document.querySelector('.cursor')
+// document.addEventListener('mousemove',(e)=>{
+//    cursor.style.left = e.pageX +'px'
+//    cursor.style.top = e.pageY +'px'
+// })
